88c32d77f1dd26c884bb126775b4fbf068fcbf30
André Junior <andre.souza64@fatec.sp.gov.br>
2021-04-30 10:15:11 -0300

    Pay stripe add

---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,76 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { query as q } from 'faunadb';
+import { getSession } from "next-auth/client";
+import { fauna } from "../../services/fauna";
+import { stripe } from "../../services/stripe";
+
+type IUser = {
+  ref: {
+    id: string;
+  };
+  data: {
+    stripe_customer_id: string;
+  }
+}
+
+export default async (request: NextApiRequest, response: NextApiResponse) => {
+  if (request.method === 'POST') {
+    const session = await getSession({
+      req: request
+    });
+
+    const user = await fauna.query<IUser>(
+      q.Get(
+        q.Match(
+          q.Index('user_by_email'),
+          q.Casefold(session.user.email)
+        )
+      )
+    );
+
+    let custumerId = user.data.stripe_customer_id;
+
+    if (!custumerId) {
+      const stripeCustumer = await stripe.customers.create({
+        email: session.user.email,
+        // metadata:
+      });
+
+      await fauna.query(
+        q.Update(
+          q.Ref(q.Collection('users'), user.ref.id),
+          {
+            data: {
+              stripe: {
+                stripe_customer_id: stripeCustumer.id
+              }
+            }
+          }
+        )
+      );
+
+      custumerId = stripeCustumer.id;
+    }
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: custumerId,
+      payment_method_types: ['card'],
+      billing_address_collection: 'required',
+      line_items: [
+        {
+          price: 'price_1IlY23KuVBsR7TIwZXL3m5b8',
+          quantity: 1
+        }
+      ],
+      mode: 'subscription',
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_SUCCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL
+    });
+
+    return response.status(200).json({ sessionId: stripeCheckoutSession.id });
+  } else {
+    response.setHeader('Allow', 'POST');
+    response.status(405).end('Method not allowed');
+  }
+}
